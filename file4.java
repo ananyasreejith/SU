2a5a31311cad409a30f1a0eede68ed6cb2677118
ananyasreejith <ananyasreejith22@gmail.com>
2024-08-29 12:52:08 +0530

    third commit

---
+++ file4.java
@@ -0,0 +1,11 @@
+import java.util.arraylist;
+class datastructure
+{
+public static void main(string []args)
+{
+arraylist.add("dobby");
+arraylist.add("kabby");
+system.out.prntln("arraylist :")
+}
+}
+
